ae4d30f0b04a3698865076294289159c1d4c9997
Jonathan Alves <113065112+JonathanBlobs@users.noreply.github.com>
2023-11-30 17:49:29 -0300

    Add files via upload
    'js adicionando prototype ao projeto'

---
+++ whats/src/controller/whatsAppController.js
@@ -0,0 +1,59 @@
+class whatsAppController {
+
+  constructor(){
+
+    console.log('whatsAppController ok');
+
+    this.elementsPrototype();
+
+    this.loadElements();
+
+    
+  }
+
+  loadElements(){
+
+    this.el = {};
+
+    document.querySelectorAll('[id]').forEach(element=>{
+
+      this.el[Format.getCamelCase(element.id)] = element;
+
+
+    });
+
+  }
+
+  elementsPrototype(){
+
+    Element.prototype.hide = function(){
+
+      this.style.display = 'none';
+
+    }
+
+    Element.prototype.show = function(){
+
+      this.style.display = 'block';
+
+    }
+
+    Element.prototype.toggle = function(){
+
+      this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+
+    }
+
+    Element.prototype.on = function(events, fn){
+
+      events.split('').forEach(event=>{
+
+        this.addEventListener(event, fn);
+
+      });
+
+    }
+
+  }
+
+}
