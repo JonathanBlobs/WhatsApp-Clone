277e7da045f8b60b11f841f10d8d86f7edc679c2
Jonathan Alves <113065112+JonathanBlobs@users.noreply.github.com>
2023-11-30 19:55:05 -0300

    prototype finalizado

--- whats/src/controller/whatsAppController.js
+++ whats/src/controller/whatsAppController.js
@@ -1,6 +1,6 @@
 class whatsAppController {
 
-  constructor(){
+  constructor() {
 
     console.log('whatsAppController ok');
 
@@ -8,14 +8,14 @@ class whatsAppController {
 
     this.loadElements();
 
-    
+
   }
 
-  loadElements(){
+  loadElements() {
 
     this.el = {};
 
-    document.querySelectorAll('[id]').forEach(element=>{
+    document.querySelectorAll('[id]').forEach(element => {
 
       this.el[Format.getCamelCase(element.id)] = element;
 
@@ -24,36 +24,78 @@ class whatsAppController {
 
   }
 
-  elementsPrototype(){
+  elementsPrototype() {
 
-    Element.prototype.hide = function(){
+    Element.prototype.hide = function () {
 
       this.style.display = 'none';
+      return this;
 
     }
 
-    Element.prototype.show = function(){
+    Element.prototype.show = function () {
 
       this.style.display = 'block';
+      return this;
 
     }
 
-    Element.prototype.toggle = function(){
+    Element.prototype.toggle = function () {
 
       this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+      return this;
 
     }
 
-    Element.prototype.on = function(events, fn){
+    Element.prototype.on = function (events, fn) {
 
-      events.split('').forEach(event=>{
+      events.split('').forEach(event => {
 
         this.addEventListener(event, fn);
 
       });
+      return this;
+
+    }
+
+    Element.prototype.css = function (styles) {
+
+      for (let name in styles) {
+
+        this.style[name] = styles[name];
+
+      }
+      return this;
+
+    }
+
+    Element.prototype.addClass = function (name) {
+
+      this.classList.add(name);
+      return this;
+
+    }
+
+    Element.prototype.removeClass = function (name) {
+
+      this.classList.remove(name);
+      return this;
+
+    }
+
+    Element.prototype.toggleClass = function (name) {
+
+      this.classList.toggle(name);
+      return this;
+
+    }
+
+    Element.prototype.hasClass = function (name) {
+
+      return this.classList.contains(name);
 
     }
 
   }
 
-}
+} 
