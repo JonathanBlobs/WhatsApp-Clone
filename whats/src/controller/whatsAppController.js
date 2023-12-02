3b4169e9eb86f322493c3dcc7c0bcf5af24fd894
Jonathan Alves <113065112+JonathanBlobs@users.noreply.github.com>
2023-12-01 21:56:00 -0300

    Adicionando eventos na foto de perfil e na conversa, obs "abrir e fechar"

--- whats/src/controller/whatsAppController.js
+++ whats/src/controller/whatsAppController.js
@@ -2,23 +2,21 @@ class whatsAppController {
 
   constructor() {
 
-    console.log('whatsAppController ok');
+    console.log('whatsAppController OK');
 
     this.elementsPrototype();
-
     this.loadElements();
-
-
+    this.initEvents();
+    
   }
 
   loadElements() {
 
     this.el = {};
 
-    document.querySelectorAll('[id]').forEach(element => {
+    document.querySelectorAll('[id]').forEach(element=>{
 
       this.el[Format.getCamelCase(element.id)] = element;
-
 
     });
 
@@ -49,27 +47,29 @@ class whatsAppController {
 
     Element.prototype.on = function (events, fn) {
 
-      events.split('').forEach(event => {
+      events.split(' ').forEach(event=>{
 
         this.addEventListener(event, fn);
 
       });
+
       return this;
 
     }
 
-    Element.prototype.css = function (styles) {
+    Element.prototype.css = function(styles){
 
       for (let name in styles) {
 
         this.style[name] = styles[name];
 
       }
+
       return this;
 
     }
 
-    Element.prototype.addClass = function (name) {
+    Element.prototype.addClass = function(name){
 
       this.classList.add(name);
       return this;
@@ -95,6 +95,47 @@ class whatsAppController {
       return this.classList.contains(name);
 
     }
+
+  }
+
+  initEvents(){
+
+    this.el.myPhoto.on('click', e=>{
+
+      this.closeAllLeftPanel();
+
+      this.el.panelEditProfile.show();
+      app.el.panelEditProfile.addClass('open');
+
+    });
+
+    this.el.btnNewContact.on('click', e =>{
+
+      this.closeAllLeftPanel();
+
+      this.el.panelAddContact.show();
+      app.el.panelAddContact.addClass('open');
+
+    });
+
+    this.el.btnClosePanelEditProfile.on('click', e=>{
+
+      this.el.panelEditProfile.removeClass('open');
+
+    });
+
+    this.el.btnClosePanelAddContact.on('click', e=>{
+
+      this.el.panelAddContact.removeClass('open');
+
+    });
+
+  }
+
+  closeAllLeftPanel(){
+
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
 
   }
 
