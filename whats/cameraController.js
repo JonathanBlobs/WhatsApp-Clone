78ffff25c8d9b79771f9b23c1a5659cb78d49545
Jonathan Alves <113065112+JonathanBlobs@users.noreply.github.com>
2023-12-13 15:47:29 -0300

    Adicionando camera ao projeto

---
+++ whats/cameraController.js
@@ -0,0 +1,23 @@
+class cameraController {
+
+  constructor(videoEl) {
+
+    this._videoEl = videoEl;
+
+    navigator.mediaDevices.getUserMedia({
+      video: true
+
+    }).then(stream => {
+
+      this._videoEl.src = URL.createObjectURL(stream);
+      this._videoEl.play();
+
+    }).catch(err=>{
+
+      console.error(err);
+      
+    });
+
+  }
+
+}
